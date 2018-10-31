e298278dd4372205831c6210b416957fde1cfb12
Patrick L Krsnak <40204902+pkrsnak@users.noreply.github.com>
2018-10-31 13:18:16 -0500

    Update amazon_asin_fee.sql
    Amazon fixes

--- amazon_asin_fee.sql
+++ amazon_asin_fee.sql
@@ -957,3 +957,25 @@ AND      c.CUSTOMER_NBR_STND = 634001
 ----------------------------------------------------------------------------------------------------SUPPLIES?
 ;
 
+
+--forecast extract
+SELECT   RUN_DATE,
+         FC_ID,
+         SN_CODE,
+         ITEMS_PER_INNER_PACK,
+         COST_AMOUNT,
+         PRODUCT_TIER_ID,
+         UPC,
+         UPC_ALT1,
+         UPC_ALT2,
+         UPC_ALT3,
+         UPC_ALT4,
+         UPC_ALT5,
+         PARENT_ASIN,
+         ITEM_NBR_HS,
+         ITEM_NBR_HS_AMZ,
+         ITEM_NAME,
+         FORECAST_OVERRIDE_FLG,
+         FORECAST_WEEK,
+         decrypt_char( FORECAST_AMT, 'Test Phrase') forecast_amt
+FROM     CRMADMIN.T_WHSE_AMAZON_FORECAST;

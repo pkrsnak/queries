dcdbb21678ea02a00818e3ddfc5623e65a9c6578
Patrick L Krsnak <40204902+pkrsnak@users.noreply.github.com>
2019-12-02 10:47:29 -0600

    kpi updates

--- item_stats.sql
+++ item_stats.sql
@@ -10,11 +10,12 @@ SELECT   istat.FISCAL_YEAR,
 --         istat.ITEM_NBR,
          sum(case when istat.ITEM_ADDED_YEAR = istat.FISCAL_YEAR then 1 else 0 end) item_added,
          sum(istat.SALES) total_sales,
-         sum(case when istat.SALES_AMZ > 1000 then 1 else 0 end) amazon_count,
-         sum(case when istat.SALES_DG > 1000 then 1 else 0 end) dg_count,
-         sum(case when istat.SALES_CORP > 1000 then 1 else 0 end) corp_count,
-         sum(case when istat.SALES_OTHER > 1000 then 1 else 0 end) ind_count
-FROM     ( SELECT DATE_PART('YEAR', dsh.TRANSACTION_DATE) fiscal_year, dsh.FACILITY_ID, f.FACILITY_NAME, dsh.SHIP_FACILITY_ID, fs.FACILITY_NAME SHIP_FACILITY_NAME, dsh.WHSE_CMDTY_ID, dsh.COMMODITY_CODE, dsh.WHOLESALE_DEPT_ID, dsh.ITEM_NBR, DATE_PART('YEAR', i.ITEM_ADDED_DATE) item_added_year, sum(dsh.TOTAL_SALES_AMT) sales, sum(case when c.CORPORATION_ID = 634001 then dsh.TOTAL_SALES_AMT else 0 end) sales_amz, sum(case when c.CORPORATION_ID = 910022 then dsh.TOTAL_SALES_AMT else 0 end) sales_dg, sum(case when c.CORPORATION_ID = 3999 then dsh.TOTAL_SALES_AMT else 0 end) sales_corp, sum(case when c.CORPORATION_ID not in (634001, 910022, 3999) then dsh.TOTAL_SALES_AMT else 0 end) sales_other FROM WH_OWNER.DC_SALES_HST dsh 
+         sum(case when istat.SALES_AMZ > 0 then 1 else 0 end) amazon_count,
+         sum(case when istat.SALES_DG > 0 then 1 else 0 end) dg_count,
+         sum(case when istat.SALES_CORP > 0 then 1 else 0 end) corp_count,
+         sum(case when istat.SALES_OTHER > 0 then 1 else 0 end) ind_count,
+         sum(case when istat.SALES > 0 then 1 else 0 end) total_count
+FROM     ( SELECT DATE_PART('YEAR', dsh.TRANSACTION_DATE) fiscal_year, dsh.FACILITY_ID, f.FACILITY_NAME, dsh.SHIP_FACILITY_ID, fs.FACILITY_NAME SHIP_FACILITY_NAME, dsh.WHSE_CMDTY_ID, dsh.COMMODITY_CODE, dsh.WHOLESALE_DEPT_ID, dsh.ITEM_NBR, DATE_PART('YEAR', i.ITEM_ADDED_DATE) item_added_year, sum(dsh.TOTAL_SALES_AMT) sales, sum(case when c.CORPORATION_ID = 634001 then dsh.TOTAL_SALES_AMT else 0 end) sales_amz, sum(case when c.CORPORATION_ID = 910022 then dsh.TOTAL_SALES_AMT else 0 end) sales_dg, sum(case when c.CORPORATION_ID in (118, 3999) then dsh.TOTAL_SALES_AMT else 0 end) sales_corp, sum(case when c.CORPORATION_ID not in (634001, 910022, 3999) then dsh.TOTAL_SALES_AMT else 0 end) sales_other FROM WH_OWNER.DC_SALES_HST dsh 
          inner join WH_OWNER.DC_FACILITY f on dsh.FACILITY_ID = f.FACILITY_ID 
          inner join WH_OWNER.DC_FACILITY fs on dsh.SHIP_FACILITY_ID = fs.FACILITY_ID 
          inner join WH_OWNER.DC_ITEM i on dsh.FACILITY_ID = i.FACILITY_ID and dsh.ITEM_NBR = i.ITEM_NBR 
@@ -35,12 +36,20 @@ SELECT   year(lh.LAYER_FILE_DTE) fiscal_year,
          int(lh.FACILITYID) FACILITY_ID,
          int(lh.STOCK_FAC) SHIP_FACILITY_ID,
          int(lh.ITEM_NBR_HS) item_nbr,
-         int(lh.ITEM_DEPT) WHOLESALE_DEPT_ID,
+         int(lh.ITEM_DEPT) WHOLESALE_DEPT_ID, i.COMMODITY_XREF, i.ITEM_DEPT,
          sum(lh.INVENTORY_TURN + lh.INVENTORY_PROMOTION + lh.INVENTORY_FWD_BUY) inventory
 FROM     CRMADMIN.T_WHSE_LAYER_HISTORY lh 
          inner join CRMADMIN.T_WHSE_DIV_XREF dx on lh.FACILITYID = dx.SWAT_ID
+         inner join CRMADMIN.T_WHSE_ITEM i on i.FACILITYID = lh.FACILITYID and i.ITEM_NBR_HS = lh.ITEM_NBR_HS
 WHERE    lh.LAYER_FILE_DTE in ('2016-12-31', '2017-12-31', '2018-12-31', '2019-11-22')
 AND      dx.ENTERPRISE_KEY = 1
 GROUP BY year(lh.LAYER_FILE_DTE), lh.FACILITYID, lh.STOCK_FAC, lh.ITEM_NBR_HS, 
-         lh.ITEM_DEPT
+         lh.ITEM_DEPT, i.COMMODITY_XREF, i.ITEM_DEPT
 HAVING   sum(lh.INVENTORY_TURN + lh.INVENTORY_PROMOTION + lh.INVENTORY_FWD_BUY) > 0
+;
+
+
+SELECT FACILITYID, ITEM_DEPT, COMMODITY_XREF, WAREHOUSE_CODE, ITEM_ADDED_DATE, count(*) num_items
+FROM     CRMADMIN.T_WHSE_ITEM
+group by FACILITYID, ITEM_DEPT, COMMODITY_XREF, WAREHOUSE_CODE, ITEM_ADDED_DATE
+;
--- po_exceptions_factors_mdv.sql
+++ po_exceptions_factors_mdv.sql
@@ -1,91 +1,89 @@
 SELECT   mi.dept_cd,
          mi.case_upc_cd,
-         mi.broker_nbr,
-         mi.export_broker_nbr,
-         mi.customer_nbr,
-         mi.vendor_id,
-         mi.ap_vendor_id,
-         mi.item_desc,
-         mi.item_pack_qty,
-         mi.item_size_desc,
-         mi.prod_group_cd,
-         mi.commodity_cd,
-         mi.lbs_msr,
-         mi.net_msr,
-         mi.cube_msr,
-         mi.master_pack_flg,
-         mi.item_status_cd,
-         mi.purchase_rank_cd,
-         mi.rstr_subst_list_cd,
-         mi.uom_cd,
-         mi.catch_wgt_flg,
-         mi.case_cost_amt,
-         mi.sell_price_amt,
-         mi.fee_amt,
-         mi.fee_method_cd,
-         mi.birth_date,
-         mi.last_sale_date,
-         mi.last_po_date,
-         mi.deca_phs_date,
-         mi.last_receipt_date,
-         mi.item_upc_cd,
-         mi.expanded_upc_cd,
-         mi.item_nbr,
-         mi.shipping_gtin_nbr,
-         mi.item_gtin_nbr,
-         mi.purchase_gtin_nbr,
-         mi.season_cd,
-         mi.upstream_flg,
-         mi.abc_class_cd,
-         mi.po_restrict_cd,
-         mi.code_date_flg,
-         mi.econ_order_qty,
-         mi.lead_time_id,
-         mi.eff_econ_order_qty,
-         mi.customer_grp_nbr,
-         mi.exp_po_upc_cd,
-         mi.exp_bill_upc_cd,
-         mi.std_prod_cost_amt,
-         mi.std_item_cost_amt,
-         mi.std_item_sell_amt,
-         mi.item_delete_flg,
-         mi.buyer_delete_flg,
-         mi.ps_prod_cd,
-         mi.private_label_desc,
-         mi.inventory_val_amt,
-         mi.on_hand_qty,
          mi.buyer_id,
-         mi.acctmgr_id,
-         mi.slot_id,
-         mi.shelf_life_nbr,
-         mi.forecast_qty,
+         mi.case_cost_amt,
+         mi.item_desc,
+         mi.season_cd,
+         mi.i_o_flg,
+         mi.lead_time_id,
          mi.safety_stock_qty,
-         mi.extr_sfystk_wk_nbr,
-         mi.on_order_qty,
-         mi.order_point_qty,
+         mi.forecast_qty,
+         mi.on_hand_qty,
          mi.reserved_qty,
          mi.advanced_order_qty,
-         mi.xdock_on_hand_qty,
-         mi.xdock_on_order_qty,
-         mi.min_layers_nbr,
-         mi.low_po_qty,
-         mi.deal_qty, poh.order_date,
-         pod.discount_price_amt,
-         pod.initials_txt,
+         mi.order_point_qty,
+         mi.on_order_qty,
+         mi.econ_order_qty,
+         mi.moq,
+         mi.shelf_life_nbr,
+         mi.poq,
+         mi.max_inventory_cases,
+         mi.max_inventory,
+         mi.excess_inventory_cases_chk,
          pod.input_date,
          pod.line_nbr,
          pod.order_nbr,
          pod.order_status_cd,
          pod.case_pack_msr,
-         pod.case_upc_cd,
          pod.order_qty,
-         pod.order_denied_qty,
          pod.reference_cd,
-         pod.received_qty,
-         pod.vendor_cd,
-         pod.line_weight_msr,
-         pod.distributed_qty
-FROM     whmgr.mdv_item mi 
-         inner join whmgr.mdv_po_dtl pod on pod.dept_cd = mi.dept_cd and pod.case_upc_cd = mi.case_upc_cd
-         inner join whmgr.mdv_po_hdr poh on poh.order_nbr = pod.order_nbr
-where poh.order_date = '11-13-2019'
+         pod.line_weight_msr, 
+        case when mi.i_o_flg = 1 then 0 else case when mi.excess_inventory_cases_chk - mi.max_inventory_cases > 0 then 1 else 0 end end excess_check, 
+        case when mi.forecast_qty = 0 then 0 else case when ((pod.order_qty - (mi.advanced_order_qty + mi.reserved_qty + mi.xdock_on_order_qty + mi.deal_qty)) / mi.forecast_qty) > mi.shelf_life_nbr then case when pod.order_qty > mi.moq then 1 else 0 end else 0 end end code_date_check,
+        case when mi.i_o_flg = 1 AND (mi.on_hand_qty + mi.on_order_qty - pod.order_qty) - (mi.reserved_qty + mi.advanced_order_qty + mi.xdock_on_order_qty + mi.xdock_on_hand_qty + mi.deal_qty) > 0  THEN 1 ELSE 0 END i_o_check
+FROM     whmgr.mdv_po_dtl pod 
+         inner join (SELECT i.dept_cd, i.case_upc_cd, i.buyer_id, i.case_cost_amt, i.item_desc, i.season_cd, case when i.rstr_subst_list_cd in ('O1', 'O2', 'O3', 'O4', 'O5') THEN 1 else 0 end I_O_FLG, i.lead_time_id, i.safety_stock_qty, i.forecast_qty, i.on_hand_qty, i.reserved_qty, i.advanced_order_qty, i.order_point_qty, i.on_order_qty, i.econ_order_qty, i.eff_econ_order_qty moq, i.xdock_on_order_qty, i.xdock_on_hand_qty, i.deal_qty, i.shelf_life_nbr, i.deal_qty poq, (i.reserved_qty + i.advanced_order_qty + i.order_point_qty + i.deal_qty + i.xdock_on_hand_qty + ((i.lead_time_id / 7)) * i.forecast_qty) MAX_INVENTORY_CASES, (i.reserved_qty + i.advanced_order_qty + i.order_point_qty + i.deal_qty + i.xdock_on_hand_qty + ((i.lead_time_id / 7) * i.forecast_qty)) * i.case_cost_amt MAX_INVENTORY, ((i.on_hand_qty + i.on_order_qty) - (i.order_point_qty + i.deal_qty + i.advanced_order_qty + i.xdock_on_hand_qty + i.xdock_on_order_qty + i.reserved_qty)) - (i.forecast_qty * .5) EXCESS_INVENTORY_CASES_CHK FROM whmgr.mdv_item i) mi on pod.dept_cd = mi.dept_cd and pod.case_upc_cd = mi.case_upc_cd
+WHERE    pod.input_date = '11-15-2019'
+;
+
+
+SELECT   i.dept_cd,
+         i.case_upc_cd,
+         i.buyer_id,
+         i.case_cost_amt,
+         i.item_desc,
+         i.season_cd,
+         case 
+              when i.rstr_subst_list_cd in ('O1', 'O2', 'O3', 'O4', 'O5') THEN 1 
+              else 0 
+         end I_O_FLG,
+         i.lead_time_id,
+         i.safety_stock_qty,
+         i.forecast_qty,
+         i.on_hand_qty, i.on_order_qty,
+         i.reserved_qty,
+         i.advanced_order_qty,
+         i.order_point_qty,
+         i.on_order_qty,
+         i.econ_order_qty,
+         i.eff_econ_order_qty moq,
+         i.xdock_on_order_qty, i.xdock_on_hand_qty,
+         i.deal_qty,
+         i.shelf_life_nbr,
+         i.deal_qty poq,
+         (i.reserved_qty + i.advanced_order_qty + i.order_point_qty + i.deal_qty + i.xdock_on_hand_qty + ((i.lead_time_id / 7)) * i.forecast_qty) MAX_INVENTORY_CASES,
+         (i.reserved_qty + i.advanced_order_qty + i.order_point_qty + i.deal_qty + i.xdock_on_hand_qty + ((i.lead_time_id / 7) * i.forecast_qty)) * i.case_cost_amt MAX_INVENTORY,
+         ((i.on_hand_qty + i.on_order_qty) - (i.order_point_qty + i.deal_qty + i.advanced_order_qty + i.xdock_on_hand_qty + i.xdock_on_order_qty + i.reserved_qty)) - (i.forecast_qty * .5) EXCESS_INVENTORY_CASES_CHK,
+         case when ((i.on_hand_qty) - (i.order_point_qty + i.deal_qty + i.advanced_order_qty + i.xdock_on_hand_qty + i.reserved_qty)) - (i.forecast_qty * .5) > 0 then ((i.on_hand_qty) - (i.order_point_qty + i.deal_qty + i.advanced_order_qty + i.xdock_on_hand_qty + i.reserved_qty)) - (i.forecast_qty * .5) else 0 end EXCESS_INVENTORY_CASES,
+         case when ((i.on_hand_qty) - (i.order_point_qty + i.deal_qty + i.advanced_order_qty + i.xdock_on_hand_qty + i.reserved_qty)) - (i.forecast_qty * .5) > 0 then ((i.on_hand_qty) - (i.order_point_qty + i.deal_qty + i.advanced_order_qty + i.xdock_on_hand_qty + i.reserved_qty)) - (i.forecast_qty * .5) else 0 end * i.case_cost_amt EXCESS_INVENTORY_CASES
+FROM     whmgr.mdv_item i
+;
+
+
+--         max(((i.INVENTORY_TOTAL + i.ON_ORDER_TOTAL) - (i.ORDER_POINT + i.IN_PROCESS_REGULAR + i.IN_PROCESS_PROMO + i.POQ_CURR + i.RESERVE_COMMITTED + i.RESERVE_UNCOMMITTED + i.STORAGE_COMMITTED + i.STORAGE_UNCOMMITTED + (ceiling(((case when i.ORDER_INTERVAL_WEEKS = 0 then 1 else i.ORDER_INTERVAL_WEEKS end) * 7) * (double(nvl(i.CASES_PER_WEEK, 0)) / 7))) + i.INVENTORY_PROMOTION + i.INVENTORY_FWD_BUY + pod.FORWARD_BUY + pod.PROMOTION)) - ceiling(i.CASES_PER_WEEK * .5), 0) * i.LIST_COST EXCESS_INVENTORY_CHK,
+
+--        case when i.I_O_FLAG = 1 then 0 else case when ( max(((i.INVENTORY_TOTAL + i.ON_ORDER_TOTAL) - (i.ORDER_POINT + i.IN_PROCESS_REGULAR + i.IN_PROCESS_PROMO + i.POQ_CURR + i.RESERVE_COMMITTED + i.RESERVE_UNCOMMITTED + i.STORAGE_COMMITTED + i.STORAGE_UNCOMMITTED + (ceiling(((case when i.ORDER_INTERVAL_WEEKS = 0 then 1 else i.ORDER_INTERVAL_WEEKS end) * 7) * (double(nvl(i.CASES_PER_WEEK, 0)) / 7))) + i.INVENTORY_PROMOTION + i.INVENTORY_FWD_BUY + pod.FORWARD_BUY + pod.PROMOTION)) - ceiling(i.CASES_PER_WEEK * .5), 0) - i.MAX_INVENTORY_CASES) > 0 then 1 else 0 end end excess_check,
+
+--case when (po.on_order_qty - (i.advanced_order_qty + i.reserved_qty + i.xdock_on_order_qty + i.deal_qty) / i.forecast_qty) > i.shelf_life_nbr then case when po.on_order_qty > i.eff_econ_order_qty then 1 else 0 end code_date_check,
+
+
+         case when (case when nvl(i.CASES_PER_WEEK, 0) = 0 then 0 else (case when (floor(((pod.TURN + pod.FORWARD_BUY) - i.IN_PROCESS_REGULAR - i.IN_PROCESS_PROMO - i.RESERVE_COMMITTED - i.RESERVE_UNCOMMITTED - i.STORAGE_COMMITTED - i.STORAGE_UNCOMMITTED - poq_curr) / (double(i.CASES_PER_WEEK) / 7)) > i.PRODUCT_DATING) then 1 else 0 end) end) = 1 then (case when MFG_MIN_ORDER_QTY = 0 then 0 else (case when int((pod.TURN + pod.FORWARD_BUY) / MFG_MIN_ORDER_QTY) <> 1 then 1 else 0 end) end) else 0 end code_date_check,
+
+         CASE WHEN i.I_O_FLAG = 1 AND (i.on_hand_qty + i.on_order_qty - po.on_order_qty) - (i.reserved_qty + i.advanced_order_qty + i.xdock_on_order_qty + i.xdock_on_hand_qty + i.deal_qty) > 0  THEN 1 ELSE 0 END i_o_check
+
+
+
+
+
+
+SELECT i.dept_cd, i.case_upc_cd, i.buyer_id, i.case_cost_amt, i.item_desc, i.season_cd, case when i.rstr_subst_list_cd in ('O1', 'O2', 'O3', 'O4', 'O5') THEN 1 else 0 end I_O_FLG, i.lead_time_id, i.safety_stock_qty, i.forecast_qty, i.on_hand_qty, i.reserved_qty, i.advanced_order_qty, i.order_point_qty, i.on_order_qty, i.econ_order_qty, i.eff_econ_order_qty moq, i.shelf_life_nbr, i.deal_qty poq, (i.reserved_qty + i.advanced_order_qty + i.order_point_qty + i.deal_qty + i.xdock_on_hand_qty + ((i.lead_time_id / 7)) * i.forecast_qty) MAX_INVENTORY_CASES, (i.reserved_qty + i.advanced_order_qty + i.order_point_qty + i.deal_qty + i.xdock_on_hand_qty + ((i.lead_time_id / 7) * i.forecast_qty)) * i.case_cost_amt MAX_INVENTORY, ((i.on_hand_qty + i.on_order_qty) - (i.order_point_qty + i.deal_qty + i.advanced_order_qty + i.xdock_on_hand_qty + i.xdock_on_order_qty + i.reserved_qty)) - (i.forecast_qty * .5) EXCESS_INVENTORY_CASES_CHK FROM whmgr.mdv_item i

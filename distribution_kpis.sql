b0f58cd9dc0bab5d4bc04998b114de2a1b6f2feb
Patrick L Krsnak <40204902+pkrsnak@users.noreply.github.com>
2020-01-10 16:44:12 -0600

    kpi updates

--- distribution_kpis.sql
+++ distribution_kpis.sql
@@ -240,6 +240,88 @@ where FACILITY_ID <> '999'
 group by 3, 4, 5
 ;
 
+/*
+--netezza
+--total labor hours
+SELECT   'distribution' SCORECARD_TYPE,
+         'labor_total_hours' KPI_TYPE,
+         FISCAL_WEEK_ID DATE_VALUE,  --need end date, not weekid
+         DIVISION_ID,
+--         FACILITYID FACILITY_ID,
+         FACILITY_ID KEY_VALUE,
+         sum(HRS_QTY) DATA_VALUE,
+         'F' DATA_GRANULARITY,
+         'W' TIME_GRANULARITY
+
+from (
+
+SELECT   dwl.FISCAL_WEEK_ID,
+         case when hl.S_DIVISION_CD = 'MDV' then 3 else case when hl.S_REGION_CD in ('BRT', 'CAITO') then 4 else 2 end end division_id,
+         case 
+              when dwl.LOCATION_CD = '2007' then '008' 
+              when dwl.LOCATION_CD = '2016' then '016' 
+              when dwl.LOCATION_CD = '2037' then '003' 
+              when dwl.LOCATION_CD = '2038' then '003' 
+              when dwl.LOCATION_CD = '2040' then '040' 
+              when dwl.LOCATION_CD = '2052' then '002' 
+              when dwl.LOCATION_CD = '2054' then '054' 
+              when dwl.LOCATION_CD = '2058' then '058' 
+              when dwl.LOCATION_CD = '2067' then '067' 
+              when dwl.LOCATION_CD = '2071' then '071' 
+              when dwl.LOCATION_CD = '2115' then '115' 
+              when dwl.LOCATION_CD = '2165' then '165' 
+              when dwl.LOCATION_CD = '2170' then '170' 
+              when dwl.LOCATION_CD = '2185' then '185' 
+              when dwl.LOCATION_CD = '2915' then '015' 
+              when dwl.LOCATION_CD = '2917' then '061' 
+              when dwl.LOCATION_CD = 'GGM' then '001' 
+              when dwl.LOCATION_CD = 'GGR' then '001' 
+              when dwl.LOCATION_CD = 'GPR' then '001' 
+              when dwl.LOCATION_CD = 'VSPT' then '001' 
+              when dwl.LOCATION_CD = '6922' then '069' 
+              when dwl.LOCATION_CD = '6924' then '070' 
+              when dwl.LOCATION_CD = '6927' then '027' 
+              when dwl.LOCATION_CD = '6929' then '029' --case when dwl.DEPT_ID in ('1479', '1476', '1472') then '090' else '029' end 
+              when dwl.LOCATION_CD = '6933' then '033' --case when dwl.DEPT_ID in ('1492', '1496', '1480') then '080' else '033' end
+              when dwl.LOCATION_CD = '6938' then '038' 
+              when dwl.LOCATION_CD = '6939' then '039' 
+              when dwl.LOCATION_CD = 'S6924' then '070' 
+              else '999' 
+         end facility_id,
+         hd.OPERATING_UNIT_CD,
+--         ou.OP_UNIT_DESC,
+         dwl.LOCATION_CD,
+         hl.LOCATION_DESC,
+         hl.S_DIVISION_CD,
+         hl.S_REGION_CD,
+         hl.S_DISTRICT_CD,
+         hl.S_BANNER_CD,
+         dwl.DEPT_ID, hd.S_GL_DEPT_ID, hd.S_GL_DEPT_ID_DESC, 
+         hd.DEPT_DESC,
+         dwl.EARNINGS_CD,
+         dwl.PAY_GROUP_CD,
+         dwl.JOB_CD,
+         hj.JOB_CD_DESC, 
+         case when dwl.OVERTIME_HRS_QTY <> 0 then 'OT' else 'RG' end overtime,
+         dwl.EARNINGS_AMT,
+         dwl.HRS_QTY,
+         dwl.OVERTIME_HRS_QTY,
+         dwl.LOAD_BATCH_ID,
+         dwl.ORIGIN_ID
+FROM     WH_OWNER.DCLBR_WK_LOC dwl 
+         inner join WH_OWNER.PS_HR_LOCATION hl on dwl.LOCATION_CD = hl.LOCATION_CD 
+         inner join WH_OWNER.PS_HR_DEPT hd on dwl.DEPT_ID = hd.DEPT_ID 
+         inner join WH_OWNER.PS_HR_JOB hj on dwl.JOB_CD = hj.JOB_CD 
+--         inner join WH_OWNER.PS_HR_EARNINGS he on dwl.EARNINGS_CD = he.EARNINGS_CD 
+--         inner join wh_owner.PS_HR_OPER_UNIT ou on hd.OPERATING_UNIT_CD = ou.OPERATING_UNIT_CD
+WHERE    FISCAL_WEEK_ID = 201949
+AND      hd.S_GL_DEPT_ID in ('8100', '8110', '8160', '8500', '8115', '8116', '8117', '8716', '8717')
+AND      hj.JOB_CD in ('3849', '3846', '0057', '5203', '5352')
+) lbr
+where FACILITY_ID <> '999'
+group by 3, 4, 5
+;
+*/
 
 --pshrdw
 --total headcount

be6c5a281a62299b1c7fc46b7ac760bea91d9474
Patrick L Krsnak <40204902+pkrsnak@users.noreply.github.com>
2021-02-01 16:42:22 -0600

    updates

--- kpi_inventory_adj_credit_issue.sql
+++ kpi_inventory_adj_credit_issue.sql
@@ -1,6 +1,4 @@
-Hi Pat,
- 
-Below is the Inventory Adjustments SQL from PROD.
+
  
 --total abs value inventory adjustments by facility
 --source:  datawhse02 & mdvods
@@ -14,7 +12,7 @@ SELECT type,  'distribution' SCORECARD_TYPE,
          sum(KPI_DATA_VALUE) KPI_DATA_VALUE
 from
 (
-
+;
 SELECT 'adj' type,  dr.division_id,
          fd.fiscal_week_id KPI_DATE, --need week end date for prior week
          fia.facility_id KPI_KEY_VALUE,
@@ -24,7 +22,7 @@ FROM     whmgr.dc_d_fac_invctrl_adj fia
          inner join whmgr.DC_FACILITY df on fia.FACILITY_ID = df.FACILITY_ID
          inner join whmgr.DC_REGION dr on df.REGION_ID = dr.REGION_ID
 WHERE    fia.ext_layer_cost_amt <> 0
-and     fd.fiscal_week_id = 202102
+and     fd.fiscal_week_id = 202101
 AND      fia.invtry_adjust_cd in ('CC', 'FL', 'CL', 'CG', 'DS', 'MA', 'AM', 'PC', 'PI', 'RC')
 GROUP BY dr.division_id, fia.facility_id, KPI_DATE
 
@@ -40,7 +38,7 @@ FROM     whmgr.mdv_d_fac_invctrl_adj mia
          inner join whmgr.DC_FACILITY df on mia.FACILITY_ID = df.FACILITY_ID
          inner join whmgr.DC_REGION dr on df.REGION_ID = dr.REGION_ID
 WHERE    mia.ext_layer_cost_amt <> 0
-and     fd.fiscal_week_id = 202102
+and     fd.fiscal_week_id = 202101
 AND      mia.facility_id in (80, 90)
 AND      mia.invtry_adjust_cd in ('CC', 'FL', 'CL', 'CG', 'DS', 'MA', 'AM', 'PC', 'PI', 'RC')
 GROUP BY dr.division_id, mia.facility_id, KPI_DATE
@@ -57,7 +55,7 @@ FROM     whmgr.mdv_d_fac_invctrl_adj mia
          inner join whmgr.DC_FACILITY df on mia.FACILITY_ID = df.FACILITY_ID
          inner join whmgr.DC_REGION dr on df.REGION_ID = dr.REGION_ID
 WHERE    mia.ext_layer_cost_amt <> 0
-and     fd.fiscal_week_id = 202102
+and     fd.fiscal_week_id = 202101
 and      mia.facility_id not in (80, 90)
 AND      mia.invtry_adjust_cd in ('CC', 'FL', 'CL', 'CG', 'DS', 'MA', 'AM', 'PC', 'PI', 'RC')
 GROUP BY dr.division_id, mia.facility_id, KPI_DATE
@@ -73,7 +71,7 @@ FROM     whmgr.DC_SALES_HST dsh
          inner join whmgr.FISCAL_DAY fd on dsh.TRANSACTION_DATE = fd.SALES_DT
          inner join whmgr.DC_FACILITY df on dsh.SHIP_FACILITY_ID = df.FACILITY_ID
          inner join whmgr.DC_REGION dr on df.REGION_ID = dr.REGION_ID
-WHERE    fd.fiscal_week_id = 202102
+WHERE    fd.fiscal_week_id = 202101
 AND      ((dsh.FACILITY_ID <> 1
       --  AND dsh.CREDIT_REASON_CD in ('01', '03', '05', '08', '09', '10', '11', '12', '19', '20', '21', '22', '26', '29', '40''43', '44', '45', '48','AR', 'BE', 'MS', 'RT', 'SH'))
           AND dsh.CREDIT_REASON_CD in ('01', '03', '05', '08', '09', '10', '11', '12', '19', '20', '21', '22', '26', '29', '40''43', '44', '45', '48'))
@@ -92,7 +90,7 @@ FROM     whmgr.mdv_fd_cr_hst dsh
          inner join whmgr.FISCAL_DAY fd on dsh.INPUT_DATE = fd.SALES_DT
          inner join whmgr.DC_FACILITY df on dsh.FACILITY_ID = df.FACILITY_ID
          inner join whmgr.DC_REGION dr on df.REGION_ID = dr.REGION_ID
-WHERE    fd.fiscal_week_id = 202102
+WHERE    fd.fiscal_week_id = 202101
 AND      ((dsh.FACILITY_ID <> 1
         AND dsh.CREDIT_REASON_CD in ('AR', 'BE', 'MS', 'RT', 'SH'))
      )
@@ -109,7 +107,7 @@ FROM     whmgr.vcm_cr_hst vcm
          inner join whmgr.FISCAL_DAY fd on vcm.ship_date = fd.SALES_DT
          inner join whmgr.DC_FACILITY df on vcm.FACILITY_ID = df.FACILITY_ID
          inner join whmgr.DC_REGION dr on df.REGION_ID = dr.REGION_ID
-WHERE    fd.fiscal_week_id = 202102
+WHERE    fd.fiscal_week_id = 202101
 AND      vcm.cr_type_cd IN ('1','2','3','4','6','9','10','11','12','13','14','16','18','19','22','23','24','25','28','29','30','31','32','33','34','35','37','38','39','40','46','47','48','49','50','51','52','53','54','55','56','57','58')
 GROUP BY dr.DIVISION_ID, vcm.FACILITY_ID, KPI_DATE
  
@@ -124,11 +122,11 @@ FROM     whmgr.vcm_cr_hst vcm
          inner join whmgr.FISCAL_DAY fd on vcm.ship_date = fd.SALES_DT
          inner join whmgr.DC_FACILITY df on vcm.FACILITY_ID = df.FACILITY_ID
          inner join whmgr.DC_REGION dr on df.REGION_ID = dr.REGION_ID
-WHERE    fd.fiscal_week_id = 202102
+WHERE    fd.fiscal_week_id = 202101
 AND      vcm.FACILITY_ID = 27
 AND      vcm.cr_type_cd IN ('1','2','3','4','6','9','10','11','12','13','14','16','18','19','22','23','24','25','28','29','30','31','32','33','34','35','37','38','39','40','46','47','48','49','50','51','52','53','54','55','56','57','58')
 GROUP BY dr.DIVISION_ID, vcm.FACILITY_ID, KPI_DATE
- 
+; 
 ) x
 group by type, division_id, fiscal_week_id, KPI_KEY_VALUE
 ;
@@ -137,11 +135,11 @@ group by type, division_id, fiscal_week_id, KPI_KEY_VALUE
 
 SELECT   *
 FROM     whmgr.mdv_fd_cr_hst
-WHERE    input_date between '01-10-2021' and '01-16-2021'
+WHERE    input_date between '01-03-2021' and '01-09-2021'
 AND      CREDIT_REASON_CD in ('AR', 'BE', 'MS', 'RT', 'SH')
 ;
 
-
+; -- can't find on NTZ
 SELECT *
 FROM     whmgr.vcm_cr_hst 
 WHERE    ship_date between '01-10-2021' and '01-16-2021'
--- myrtle_extracts.sql
+++ myrtle_extracts.sql
@@ -1,3 +1,4 @@
+--select count(*) from (
 SELECT   i.FACILITYID,
          i.VENDOR_NBR,
          v.VENDOR_NAME,
@@ -19,8 +20,34 @@ SELECT   i.FACILITYID,
 FROM     CRMADMIN.T_WHSE_PO_DTL pod 
          inner join CRMADMIN.T_WHSE_ITEM i on pod.ITEM_FAC = i.BICEPS_DC and pod.ITEM_NBR = i.ITEM_NBR 
          inner join CRMADMIN.T_WHSE_VENDOR v on i.FACILITYID = v.FACILITYID and i.VENDOR_NBR = v.VENDOR_NBR
-WHERE    pod.ITEM_FAC = '01'
-AND      pod.DATE_ORDERED >= current date - 90 days;
+WHERE    pod.DATE_ORDERED >= current date - 90 days
+--AND      pod.ITEM_FAC = '01'
+--)
+;
+
+--select count(*) from (
+SELECT   d.division_cd,
+         pod.dept_cd,
+         poh.vendor_cd,
+         v.vendor_name,
+         pod.case_upc_cd,
+         i.item_desc,
+         i.item_pack_qty,
+         i.item_size_desc,
+         i.item_upc_cd,
+         poh.order_nbr,
+         poh.order_date,
+         poh.receive_date,
+         pod.order_qty,
+         pod.received_qty
+FROM     whmgr.mdv_po_dtl pod 
+         inner join whmgr.mdv_po_hdr poh on pod.order_nbr = poh.order_nbr 
+         inner join whmgr.mdv_item i on pod.dept_cd = i.dept_cd and pod.case_upc_cd = i.case_upc_cd 
+         inner join whmgr.mdv_dept d on i.dept_cd = d.dept_cd 
+         inner join whmgr.mdv_vendor v on i.vendor_id = v.vendor_cd
+WHERE    poh.receive_date >= date(current) - 90
+--)
+;
 
 
 --select count(*) from (
@@ -48,12 +75,38 @@ SELECT   i.FACILITYID,
          od.LINE_ITEM_ID
 FROM     CRMADMIN.T_WHSE_ORDER_DTL od 
          inner join CRMADMIN.T_WHSE_ITEM i on od.FACILITYID = i.FACILITYID and od.ITEM_NBR_HS = i.ITEM_NBR_HS
-WHERE    od.FACILITYID = '001'
-AND      od.ORDER_RECVD_DTE >= current date - 90 day
---AND      od.ORDER_RECVD_DTE = current date - 90 day
+WHERE    od.ORDER_RECVD_DTE >= current date - 90 day
+--AND      od.FACILITYID = '001'
 --)
 ;
 
+
+
+--select count(*) from (
+SELECT   d.division_cd,
+         shd.dept_cd,
+         shd.customer_nbr,
+         shd.ship_to_id,
+         v.vendor_name,
+         i.case_upc_cd,
+         i.item_desc,
+         i.item_pack_qty,
+         i.item_size_desc,
+         i.item_upc_cd,
+         shd.sales_catgy_code,
+         shd.sales_catgy_desc,
+         shd.order_qty,
+         shd.ship_qty,
+         shd.ship_date,
+         shd.order_nbr,
+         shd.order_line_nbr
+FROM     whmgr.mdv_sales_hst shd 
+         inner join whmgr.mdv_item i on shd.dept_cd = i.dept_cd and shd.upc_cd = i.case_upc_cd 
+         inner join whmgr.mdv_dept d on i.dept_cd = d.dept_cd 
+         inner join whmgr.mdv_vendor v on i.vendor_id = v.vendor_cd
+WHERE    shd.ship_date >= date(current) - 90
+--)
+;
 
 SELECT   TRANSACTION_DATE,
          FACILITY_ID,

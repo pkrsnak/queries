97c4257e32216f63b6020b0401b3601cb9709c37
Patrick L Krsnak <40204902+pkrsnak@users.noreply.github.com>
2022-06-03 07:26:13 -0500

    May updates

--- order extract - amazon.sql
+++ order extract - amazon.sql
@@ -1,3 +1,47 @@
+
+insert into SBX_IT.PATRICK_KRSNAK.T_TEMP_FAC_ITEM (select distinct FACILITYID , ITEM_NBR from SBX_IT.PATRICK_KRSNAK.T_TEMP_FAC_ITEM_WORK)
+;
+--snowflake
+SELECT   facilityid, count(*)
+
+FROM     ( 
+;
+SELECT   od.FACILITYID,
+         od.RTL_ORDER_ID,
+         od.DC_ORDER_ID,
+         od.LINE_ITEM_ID,
+         od.ITEM_NBR_HS,
+         od.CUSTOMER_NBR_STND,
+         od.ORDER_TYPE,
+         od.SHIP_DATE,
+         od.ORDER_STATUS_HS,
+         od.INVOICE_NBR,
+         od.BILLING_DATE,
+         od.QTY,
+         od.NET_QTY,
+         od.ORDER_RECVD_DTE,
+         od.RTL_PO_NUMBER,
+         od.FACILITYID_REQUESTED,
+         od.ITEM_NBR_HS_REQUESTED
+FROM     EDL.CRM.T_WHSE_ORDER_DTL od 
+         inner join EDW.FD.WEB_CUSTOMER_FAC_VW C on od.FACILITYID = c.FACILITYID and od.CUSTOMER_NBR_STND = c.CUSTOMER_NBR_STND 
+--         inner join crmadmin.T_WHSE_ITEM i on od.FACILITYID = i.FACILITYID and od.ITEM_NBR_CD = i.ITEM_NBR_CD 
+         inner join SBX_IT.PATRICK_KRSNAK.T_TEMP_FAC_ITEM tfi on tfi.FACILITYID = od.FACILITYID and tfi.ITEM_NBR = od.ITEM_NBR_HS
+WHERE    c.CORP_CODE = 634001
+AND      od.ORDER_RECVD_DTE between '2020-06-01' and CURRENT_DATE()
+--AND      od.ORDER_RECVD_DTE between '2019-12-29' and '2020-07-31'
+--AND      od.ORDER_RECVD_DTE between '2020-08-01' and '2021-01-31'
+--AND      od.ORDER_RECVD_DTE >= '2021-02-01' --and '2021-01-31'
+;
+
+)
+group by facilityid
+;
+
+
+
+
+-------------------------------------------------------------------------------------------------------------------------------------------
 SELECT   facilityid, count(*)
 
 FROM     ( 
@@ -36,43 +80,4 @@ group by facilityid
 ;
 
 
-
---snowflake
-SELECT   facilityid, count(*)
-
-FROM     ( 
-;
-SELECT   od.FACILITYID,
-         od.RTL_ORDER_ID,
-         od.DC_ORDER_ID,
-         od.LINE_ITEM_ID,
-         od.ITEM_NBR_HS,
-         od.CUSTOMER_NBR_STND,
-         od.ORDER_TYPE,
-         od.SHIP_DATE,
-         od.ORDER_STATUS_HS,
-         od.INVOICE_NBR,
-         od.BILLING_DATE,
-         od.QTY,
-         od.NET_QTY,
-         od.ORDER_RECVD_DTE,
-         od.RTL_PO_NUMBER,
-         od.FACILITYID_REQUESTED,
-         od.ITEM_NBR_HS_REQUESTED
-FROM     EDL.CRM.T_WHSE_ORDER_DTL od 
-         inner join EDW.FD.WEB_CUSTOMER_FAC_VW C on od.FACILITYID = c.FACILITYID and od.CUSTOMER_NBR_STND = c.CUSTOMER_NBR_STND 
---         inner join crmadmin.T_WHSE_ITEM i on od.FACILITYID = i.FACILITYID and od.ITEM_NBR_CD = i.ITEM_NBR_CD 
-         inner join SBX_IT.PATRICK_KRSNAK.T_TEMP_FAC_ITEM tfi on tfi.FACILITYID = od.FACILITYID and tfi.ITEM_NBR = od.ITEM_NBR_HS
-WHERE    c.CORP_CODE = 634001
-AND      od.ORDER_RECVD_DTE between '2020-06-01' and CURRENT_DATE()
---AND      od.ORDER_RECVD_DTE between '2019-12-29' and '2020-07-31'
---AND      od.ORDER_RECVD_DTE between '2020-08-01' and '2021-01-31'
---AND      od.ORDER_RECVD_DTE >= '2021-02-01' --and '2021-01-31'
-;
-
-)
-group by facilityid
-;
-
-insert into SBX_IT.PATRICK_KRSNAK.T_TEMP_FAC_ITEM (select distinct FACILITYID , ITEM_NBR from SBX_IT.PATRICK_KRSNAK.T_TEMP_FAC_ITEM_WORK)
 
---
+++ ppa_deal_extract_keith.sql
@@ -0,0 +1,21 @@
+SELECT   i.FACILITYID,
+         bd.DEAL_NBR,
+         i.ITEM_NBR_HS,
+         i.ITEM_DESCRIP,
+         i.ITEM_SIZE_DESCRIP,
+         i.UPC_CASE,
+         bd.DEAL_NBR,
+         bd.DATE_START,
+         bd.DATE_END,
+         bd.AMT_OI,
+         bd.AMT_BBACK,
+         bd.DATE_ALLOW_EFF,
+         bd.DATE_ALLOW_EXP,
+         bd.ALLOW_AMT
+FROM     CRMADMIN.T_BICEPS_DEAL bd 
+         inner join CRMADMIN.T_WHSE_ITEM i on i.FACILITYID = bd.FACILITYID and i.ITEM_NBR_CD = bd.ITEM_NBR
+WHERE    (bd.DATE_START <= '2022-05-21'
+     AND  bd.DATE_END >= '2022-04-24')
+AND      (bd.AMT_OI > 0
+     OR  bd.AMT_BBACK > 0)
+ORDER BY i.FACILITYID, i.UPC_CASE
